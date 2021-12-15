f9b17b0e7b53b5d8f265b2250302c8889fc3af38
razerhood7 <72698643+razerhood7@users.noreply.github.com>
2021-12-16 00:39:16 +0530

    Create Subset Sum Problem.cpp

---
+++ Subset Sum Problem.cpp
@@ -0,0 +1,34 @@
+class Solution{   
+public:
+    bool isSubsetSum(int N, int arr[], int sum){
+        // code here 
+        
+        int t[N+1][sum+1];
+        for(int i=0;i<=N;i++)
+        {
+            for(int j=0;j<=sum;j++)
+            {
+                if(i==0)
+                    t[i][j]=false;
+                
+                if(j==0)
+                    t[i][j]=true;
+                
+            }
+        }
+        
+        for(int i=1;i<=N;i++)
+        {
+            for(int j=1;j<=sum;j++)
+            {
+                 if(arr[i-1]<=j)
+                {
+                    t[i][j]=t[i-1][j-arr[i-1]] || t[i-1][j];
+                }
+                else
+                    t[i][j]=t[i-1][j];
+            }
+        }
+        return t[N][sum];
+    }
+};                
