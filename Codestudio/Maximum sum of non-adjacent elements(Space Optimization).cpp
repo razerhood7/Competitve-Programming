9de93273d589ef15346ff66315efe05370b3c85d
razerhood7 <72698643+razerhood7@users.noreply.github.com>
2022-02-04 15:41:49 +0530

    Create Maximum sum of non-adjacent elements(Space Optimization).cpp

---
+++ Codestudio/Maximum sum of non-adjacent elements(Space Optimization).cpp
@@ -0,0 +1,18 @@
+int maximumNonAdjacentSum(vector<int> &nums){
+    // Write your code here.
+    int n=nums.size();
+    int prev=nums[0];
+    int prev2=0;
+    for(int i=1;i<n;i++)
+    {
+        int pick=nums[i];
+        if(i>1)pick+=prev2;
+        int notpick= prev;
+        
+        int cur=max(pick,notpick);
+        
+        prev2=prev,prev=cur;
+    }
+    
+    return prev;
+}
