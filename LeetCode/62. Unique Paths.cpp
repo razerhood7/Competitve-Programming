9d7ea1c4600295082acfa90f3927a7237f4d1bbe
razerhood7 <72698643+razerhood7@users.noreply.github.com>
2022-02-09 18:05:24 +0530

    Create 62. Unique Paths.cpp

---
+++ LeetCode/62. Unique Paths.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    int solve(int m, int n, vector<vector<int>> & dp)
+    {
+        for(int i=0;i<m+1;i++)
+        {
+            for(int j=0;j<n+1;j++)
+            {
+                if(i==0 || j==0)
+                    dp[i][j]=1;
+            }
+        }
+        
+        for(int i=1;i<m+1;i++)
+        {
+            for(int j=1;j<n+1;j++)
+            {
+                int up=dp[i-1][j];
+                int left=dp[i][j-1];
+                
+                dp[i][j]=up+left;
+            }
+        }
+        
+        return dp[m][n];
+    }
+    
+public:
+    int uniquePaths(int m, int n) {
+        
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return solve(m-1,n-1,dp);
+    }
+};
