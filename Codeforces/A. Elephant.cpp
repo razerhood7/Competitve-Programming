0247c260398866333310926416ae768952c991df
razerhood7 <72698643+razerhood7@users.noreply.github.com>
2022-02-12 02:14:04 +0530

    Create A. Elephant.cpp

---
+++ Codeforces/A. Elephant.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    int a,n;
+    cin>>n;
+    a=n/5;
+    if(n%5==0)
+    {
+        cout<<a;
+    }
+    else
+
+     cout<<++a;
+    
+    return 0;
+}
