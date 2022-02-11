131476903bcd79984ab83cf192ab5092b160500c
razerhood7 <72698643+razerhood7@users.noreply.github.com>
2022-02-12 02:37:10 +0530

    Create Fit in Data Type.cpp

---
+++ Codechef/Fit in Data Type.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,x;
+        cin>>n>>x;
+        
+        if(x<=n)
+        {
+            cout<<x<<endl;
+        }
+        else
+        {
+            while(x>n)
+            x=x-n-1;
+            cout<<x<<endl;
+        }
+    }
+
+    return 0;
+}
