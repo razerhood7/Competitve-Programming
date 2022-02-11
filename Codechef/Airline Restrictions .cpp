0e920febbdc12b4feaffdfaa2fd0bb962c245688
razerhood7 <72698643+razerhood7@users.noreply.github.com>
2022-02-12 02:33:14 +0530

    Create Airline Restrictions .cpp

---
+++ Codechef/Airline Restrictions .cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int count=0;
+        int a,b,c,d,e;
+        cin>>a>>b>>c>>d>>e;
+        if(a+b<=d && c<=e || b+c<=d && a<=e || a+c <=d && b<=e)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+     
+    }
+
+    return 0;
+}
