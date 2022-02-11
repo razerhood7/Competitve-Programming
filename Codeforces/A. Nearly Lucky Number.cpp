f1771a7ea4e2a333caf5c12e03631d81ae16e186
razerhood7 <72698643+razerhood7@users.noreply.github.com>
2022-02-12 02:12:22 +0530

    Create A. Nearly Lucky Number.cpp

---
+++ Codeforces/A. Nearly Lucky Number.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n;
+    int i,count=0;
+    cin>>n;
+    while(n!=0){
+    if(n%10==4||n%10==7)   
+        {
+            count++;
+        }
+        n=n/10;
+    }
+if(count==4 || count ==7)
+{
+    cout<<"YES";
+}
+else 
+cout<<"NO";
+    return 0;
+}
