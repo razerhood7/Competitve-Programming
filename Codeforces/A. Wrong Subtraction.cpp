81795fbb89419f2204f85cf6fefc617b2eed79cb
razerhood7 <72698643+razerhood7@users.noreply.github.com>
2022-02-12 02:15:23 +0530

    Create A. Wrong Subtraction.cpp

---
+++ Codeforces/A. Wrong Subtraction.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    int n,k,i;
+    cin>>n>>k;
+    for(i=0;i<k;i++)
+        {
+            if((n%10)!=0)
+            n=n-1;
+            else if((n%10)==0)
+            n=n/10;
+        }
+        cout<<n;
+
+    return 0;
+}
