a0e0b5723e8f7276ec8dd2050cd1c266bcbff6da
razerhood7 <72698643+razerhood7@users.noreply.github.com>
2022-02-12 02:24:22 +0530

    Create Finding Square Roots.cpp

---
+++ Finding Square Roots.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int ans=sqrt(n);
+        cout<<ans<<endl;
+    }
+
+    return 0;
+}
