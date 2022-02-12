870a55efdc7bb679ff3e22891ca25095f99fd1d2
razerhood7 <72698643+razerhood7@users.noreply.github.com>
2022-02-12 17:03:35 +0530

    Create Subset Sums .cpp

---
+++ GeeksforGeeks/Subset Sums .cpp
@@ -0,0 +1,28 @@
+lass Solution
+{
+private:
+    void solve(int ind,int sum, vector<int> &arr, int N, vector<int> &ans)
+    {
+        if(ind==N)
+        {
+            ans.push_back(sum);
+            return;
+        }
+        
+        solve(ind+1,sum+arr[ind],arr,N,ans);
+        solve(ind+1,sum,arr,N,ans);
+        
+    }
+public:
+    vector<int> subsetSums(vector<int> arr, int N)
+    {
+        // Write Your Code here
+        
+        vector<int> ans;
+        solve(0,0,arr,N,ans);
+        
+        sort(ans.begin(),ans.end());
+        
+        return ans;
+    }
+};
