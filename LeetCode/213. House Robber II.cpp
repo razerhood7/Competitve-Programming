943db0990b51cb31ca2f6dcefa2e7c04078a4d28
razerhood7 <72698643+razerhood7@users.noreply.github.com>
2022-02-12 14:09:53 +0530

    Create 213. House Robber II.cpp

---
+++ LeetCode/213. House Robber II.cpp
@@ -0,0 +1,45 @@
+class Solution {
+private:
+
+
+int solve(vector<int> & arr)
+{
+    int prev=arr[0];
+    int prev2=0;
+    
+    for(int i=1;i<arr.size();i++)
+    {
+        int take=arr[i];
+        if(i>1)
+            take+=prev2;
+        int nottake=prev;
+        
+        int cur=max(take,nottake);
+        prev2=prev;
+        prev=cur;
+    }
+    return prev;
+}
+    
+public:
+    int rob(vector<int>& nums) {
+        
+    int n=nums.size();
+    vector<int> arr1;
+    vector<int> arr2;
+    
+    if(n==1)
+        return nums[0];
+        
+    for(int i=0; i<n; i++){
+        
+        if(i!=0) arr1.push_back(nums[i]);
+        if(i!=n-1) arr2.push_back(nums[i]);
+    }
+    
+    int ans1 = solve(arr1);
+    int ans2 = solve(arr2);
+    
+    return max(ans1,ans2);
+    }
+};
