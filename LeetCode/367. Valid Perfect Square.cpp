04916188352596e99d2d6059407d512819018752
razerhood7 <72698643+razerhood7@users.noreply.github.com>
2022-02-13 19:07:35 +0530

    Create 367. Valid Perfect Square.cpp

---
+++ LeetCode/367. Valid Perfect Square.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        
+        long long int start=0;
+        long long int end=num;
+        long long int mid;
+        
+        
+        while(start<=end)
+        {
+            mid = start + (end-start)/2;
+            
+            if(mid * mid == num)
+                return true;
+            else if(mid * mid < num)
+                start=mid+1;
+            else
+                end=mid-1;
+        }
+        
+        return false;
+    }
+};
