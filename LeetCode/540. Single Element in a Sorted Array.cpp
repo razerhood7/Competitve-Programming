ad2130220a2524494ca3bd5f2175f35e9fb65829
razerhood7 <72698643+razerhood7@users.noreply.github.com>
2022-02-15 17:23:15 +0530

    Create 540. Single Element in a Sorted Array.cpp

---
+++ LeetCode/540. Single Element in a Sorted Array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = size(nums), L = 0, R = n-1, mid;
+        while(L <= R) {
+            mid = (L + R) >> 1;
+            bool isHalfEven = (mid-L) % 2 == 0;          
+			
+            if(mid+1 < n && nums[mid] == nums[mid+1])    
+                if(isHalfEven) L = mid+2;
+                else R = mid-1;
+				
+            else if(mid && nums[mid] == nums[mid-1])     
+                if(isHalfEven) R = mid-2;
+                else L = mid+1;
+				
+            else return nums[mid];                       
+        }
+        return -1; 
+    }
+};
