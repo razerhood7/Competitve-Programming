af8cac325602f69b02df1f5b4757ad319b78aa1b
razerhood7 <72698643+razerhood7@users.noreply.github.com>
2022-03-24 18:10:16 +0530

    Create 15. Sort a Stack.cpp

---
+++ Codestudio/15. Sort a Stack.cpp
@@ -0,0 +1,30 @@
+void solve(stack<int> &stack, int num)
+{
+    if(stack.empty() || (!stack.empty()&&stack.top()<num))
+    {
+        stack.push(num);
+        return;
+    }
+    
+    int n = stack.top();
+    stack.pop();
+    
+    solve(stack, num);
+    
+    stack.push(n);
+}
+void sortStack(stack<int> &stack)
+{
+	// Write your code here
+    if(stack.empty())
+    {
+        return;
+    }
+    
+    int num = stack.top();
+    stack.pop();
+    
+    sortStack(stack);
+    
+    solve(stack,num);
+}
