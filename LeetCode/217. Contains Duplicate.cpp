8c8bfbe62f96638b7bb6ea510ff2bb768aa4f091
razerhood7 <72698643+razerhood7@users.noreply.github.com>
2022-03-29 22:00:35 +0530

    Create 217. Contains Duplicate.cpp

---
+++ LeetCode/217. Contains Duplicate.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        if(nums.size() == 1 || nums.size() == 0)
+            return false;
+        
+        sort(nums.begin(),nums.end());
+        for(int i = 1;i<nums.size();i++)
+        {
+            if(nums[i-1] == nums[i])
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
