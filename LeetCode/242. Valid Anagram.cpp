7c2194fcd35b1c12d554955f4f620c871e22de0d
razerhood7 <72698643+razerhood7@users.noreply.github.com>
2022-04-11 23:26:17 +0530

    Create 242. Valid Anagram.cpp

---
+++ LeetCode/242. Valid Anagram.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        
+        if(s.size() != t.size())
+        {
+            return false;
+        }
+        
+        int j=0;
+        for(int i=0; i<s.size(); i++)
+        {
+            if(s[i] != t[i])
+            {
+                return false;
+            }
+            
+            j++;
+        }
+        
+        return true;
+    }
+};
