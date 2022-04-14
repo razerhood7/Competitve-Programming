d1c64b9e3fa51f643e1a6ceb53847460e98a57df
razerhood7 <72698643+razerhood7@users.noreply.github.com>
2022-04-14 23:43:09 +0530

    Create 441. Arranging Coins.cpp

---
+++ LeetCode/441. Arranging Coins.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        
+                
+        int rows = 1;
+        while(n > 0) {     
+            rows++;       
+            n -= rows;   
+        }
+        return rows - 1;  
+    }
+};
